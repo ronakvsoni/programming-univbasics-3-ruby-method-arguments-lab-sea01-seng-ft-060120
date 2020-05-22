961ab9e2cab0499f27289b810e814091aad93f14
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-22 03:09:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,9 +5,11 @@
 
 
 
-def introduction name
+def introduction (name)
   puts "Hi, my name is #{name}."
 end
+
+
 
 
 def introduction_with_language (name,language)
@@ -15,11 +17,12 @@ def introduction_with_language (name,language)
 end
 
 
-
-
 def introduction_with_language_optional (name,language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+
+
 
 
 
